a296449552375105588cd580097e5063911b928d
gorigamidev <nicolasbg.89@gmail.com>
2024-09-30 10:01:46 -0500

    Fix static config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+// import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  // adapter: netlify(),
+  // output: 'static'
 });
