1e0fe3280246223cbe83bd4967f0468acc4cbd08
GorigamiDev <gorigamidev@gmail.com>
2026-01-22 11:16:12 -0500

    Improve SEO and LLMO in web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 // import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   // adapter: netlify(),
   // output: 'static'
+  site: 'https://gorigami.xyz',
+
+  integrations: [sitemap()]
 });
